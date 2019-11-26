2a74e69a729ea2755191c74faf5321946e5c3561
nokranger <anusorn.thavornporn@gmail.com>
2019-11-26 15:44:00 +0700

    ref

--- src/views/connection.js
+++ src/views/connection.js
@@ -1,4 +1,4 @@
-export let connection = new WebSocket('ws://localhost:4040')
+export let connection = new WebSocket('ws://localhost:1308')
 connection.onerror = function (error) {
   console.error('WebSocket Error ' + error)
 }
